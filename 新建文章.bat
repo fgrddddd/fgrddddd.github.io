d49ce0d6f72dedd932a7b98f621d4fd6f199514c
fgrddddd <3373975751@qq.com>
2026-07-02 23:42:39 +0800

    update

---
+++ 新建文章.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+cd /d E:\blog
+
+set /p title=请输入文章标题：
+
+call hexo new "%title%"
+start "" "C:\Users\LENOVO\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+call hexo g
+call hexo s
+
+pause
